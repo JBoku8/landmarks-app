b65c3f85994fc27dbe3ae43d2cdb45d3ff085264
Jano Bokuchava <m.prog@yahoo.com>
2024-01-29 18:28:05 +0400

    Initial Commit

---
+++ LandmarksApp/LandmarksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksAppApp.swift
+//  LandmarksApp
+//
+//  Created by Jano Bokuchava on 29.01.24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
